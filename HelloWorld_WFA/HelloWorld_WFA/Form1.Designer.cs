bb35233451d1e98cde2eaf8877007198a8cbb5ed
ijesri-school <ijesri.school@gmail.com>
2019-01-24 00:17:40 -0600

    v1.0

---
+++ HelloWorld_WFA/HelloWorld_WFA/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HelloWorld_WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SayHelloButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(252, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(327, 53);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Hello There!";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SayHelloButton
+            // 
+            this.SayHelloButton.Location = new System.Drawing.Point(252, 297);
+            this.SayHelloButton.Name = "SayHelloButton";
+            this.SayHelloButton.Size = new System.Drawing.Size(133, 34);
+            this.SayHelloButton.TabIndex = 1;
+            this.SayHelloButton.Text = "Say Hello!";
+            this.SayHelloButton.UseVisualStyleBackColor = true;
+            this.SayHelloButton.Click += new System.EventHandler(this.SayHelloButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SayHelloButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SayHelloButton;
+    }
+}
+
